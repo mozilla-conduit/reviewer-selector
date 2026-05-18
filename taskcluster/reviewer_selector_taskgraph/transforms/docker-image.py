4277026d0f67f90dcc05ef1295bc39d6f20d96cd
Olivier Mehani <omehani@mozilla.com>
2026-05-18 15:42:44 +1000

    taskcluster: add index config in transforms

--- taskcluster/reviewer_selector_taskgraph/transforms/docker-image.py
+++ taskcluster/reviewer_selector_taskgraph/transforms/docker-image.py
@@ -4,6 +4,17 @@ from taskgraph.transforms.base import TransformSequence
 
 transforms = TransformSequence()
 
+@transforms.add
+def add_index(config, tasks):
+    for task in tasks:
+        if task is not None:
+            task["index"] = task.get("index",{}).update({
+                "product": "reviewer-selector",
+                "job-name": "docker-image",
+                })
+
+        yield task
+
 
 @transforms.add
 def add_index_routes(config, tasks):
