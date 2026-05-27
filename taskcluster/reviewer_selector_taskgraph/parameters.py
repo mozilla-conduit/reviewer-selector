836821b7a82ec746f88c6ca0fabbd21a2ac0af32
Olivier Mehani <omehani@mozilla.com>
2026-05-27 18:13:48 +1000

    taskcluster: add short_head_ref to parameters, and use in indices (bug 2028701) (#11)

---
+++ taskcluster/reviewer_selector_taskgraph/parameters.py
@@ -0,0 +1,9 @@
+
+
+def decision_parameters(graph_config, parameters):
+    short_head_ref = parameters["head_ref"]
+    for prefix in ("refs/heads/", "refs/tags/"):
+        if short_head_ref.startswith(prefix):
+            short_head_ref = short_head_ref[len(prefix) :]
+            break
+    parameters["head_ref"] = short_head_ref
