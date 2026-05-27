64075ae1a1c69ef6f864c2efbaf5f454ea0897b4
Olivier Mehani <omehani@mozilla.com>
2026-05-27 17:25:05 +1000

    build: use taskcluster & taskgraph to build Docker image (bug 2028701) (#7)
     * taskcluster: taskgraph init
     * taskcluster: remove hello kind
     * taskcluster: remove docker/linux
     * drop -gcp from worker aliases
     * taskcluster: build reviewer-selector with docker-image
     * taskcluster: remove task_context transform
     * docker: add topsrcdir symlink to support non-taskcluster builds
     * docker: topsrcdir is not supported for RUN --mount
     * taskcluster: disable standard indices for now
     * docker-image: add index routes
     * taskcluster: add index config in transforms
     * Makefile: add build target
     * Makefile: add test-related targets

--- Makefile
+++ Makefile
@@ -1,3 +1,35 @@
+DOCKER_TAG=reviewer-selector
+
+build: DOCKER_ARGS=
+.PHONY: build
+build:
+	# This symlink is needed for non-TaskCluster/TaskGraph builds to succeed, ignoring extended Dockerfile syntax [0].
+	# https://taskcluster-taskgraph.readthedocs.io/en/latest/howto/docker.html#special-dockerfile-syntax
+	test -e topsrcdir || ln -sf .. topsrcdir
+	docker build -f docker/Dockerfile -t ${DOCKER_TAG} ${DOCKER_ARGS} .
+
+.PHONY: format
 format:
 	uv run ruff format
 	uv run ruff check --fix
+
+.PHONY:
+requirements: requirements-dev.txt requirements.txt
+
+requirements.txt: pyproject.toml
+	uv pip install .[dev]
+	uv run pip-compile --allow-unsafe --generate-hashes --output-file=${@}
+
+requirements-dev.txt: pyproject.toml
+	uv pip install .[dev]
+	uv run pip-compile --allow-unsafe --generate-hashes --extra=dev --output-file=${@}
+
+.PHONY: test
+test:
+	uv pip install .[dev]
+	uv run pytest tests/
+
+.PHONY: test-docker
+test-docker:
+	docker run --rm --entrypoint bash ${DOCKER_TAG} \
+		-c 'pip install -r requirements-dev.txt && pytest tests/'
---
+++ taskcluster/reviewer_selector_taskgraph/transforms/docker-image.py
@@ -0,0 +1,31 @@
+import re
+
+from taskgraph.transforms.base import TransformSequence
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def add_index_routes(config, tasks):
+    for task in tasks:
+        params = config.params
+        head_rev = params["head_rev"]
+       # Indices only allow dot-delimited groups of /[a-zA-Z0-9_!~*'()%-]+/.
+        # Sanitise the branch name accordingly.
+        head_ref = re.sub(r"[^F a-zA-Z0-9_!~*'()%-]", "_", params["head_ref"])
+
+        index_prefix = "reviewer-selector"
+        if params["tasks_for"] == "github-pull-request":
+            index_prefix = "reviewer-selector-pr"
+
+        trust_domain = config.graph_config["trust-domain"]
+        task.setdefault("routes", []).extend(
+            [
+                # XXX: We should be using `task['name']` as the last component,
+                # but it's not defined in the task dict.
+                f"index.{trust_domain}.v2.{index_prefix}.branch.{head_ref}.revision.{head_rev}.{config.kind}.reviewer-selector",
+                f"index.{trust_domain}.v2.{index_prefix}.branch.{head_ref}.latest.{config.kind}.reviewer-selector",
+            ]
+        )
+
+        yield task
