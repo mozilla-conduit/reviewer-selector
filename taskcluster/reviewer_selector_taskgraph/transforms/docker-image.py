7520dfc3a57fe642ca1615d73b0862249b7d203f
Olivier Mehani <omehani@mozilla.com>
2026-05-27 18:33:10 +1000

    wip

--- taskcluster/reviewer_selector_taskgraph/transforms/docker-image.py
+++ taskcluster/reviewer_selector_taskgraph/transforms/docker-image.py
@@ -12,7 +12,7 @@ def add_index_routes(config, tasks):
         head_rev = params["head_rev"]
         # Indices only allow dot-delimited groups of /[a-zA-Z0-9_!~*'()%-]+/.
         # Sanitise the branch name accordingly.
-        head_ref = re.sub(r"[^F a-zA-Z0-9_!~*'()%-]", "_", params["head_ref"])
+        head_ref = "does_this_thing_work"
 
         index_prefix = "reviewer-selector"
         if params["tasks_for"] == "github-pull-request":
