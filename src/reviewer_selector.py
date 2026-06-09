81271ec4247156d964a1d0a11621a59b35363341
Olivier Mehani <omehani@mozilla.com>
2026-06-09 15:32:53 +1000

    fixup! fixup! reviewer-selector: improve info/debug logging

--- src/reviewer_selector.py
+++ src/reviewer_selector.py
@@ -157,7 +157,6 @@ def main() -> None:
 
     rules = Rules.from_file(args.rules_file)
 
-    patch = Patch(sys.stdin.read())
 
     reviewers: Iterable[Reviewer] = rules.collect_reviewers(patch, args.repo)
 
