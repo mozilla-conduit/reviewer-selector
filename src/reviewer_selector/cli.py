c697d759f607a18b2e2ae1f62785b0f3ad976418
Olivier Mehani <omehani@mozilla.com>
2026-07-06 17:10:23 +1000

    Update src/reviewer_selector/cli.py
    Co-authored-by: Connor Sheehan <sheehan@mozilla.com>

--- src/reviewer_selector/cli.py
+++ src/reviewer_selector/cli.py
@@ -78,7 +78,7 @@ def resolve_github_credentials(args: argparse.Namespace) -> tuple[str, str] | No
         tc_secret_id := (args.taskcluster_secret_id or os.environ.get("TC_SECRET_ID"))
     ):
         logger.debug(
-            f"Fetching GitHup app credentials from TC_SECRET_ID {tc_secret_id} ..."
+            f"Fetching GitHub app credentials from TC_SECRET_ID {tc_secret_id} ..."
         )
         tc = Taskcluster()
         tc_secret = tc.fetch_secret(tc_secret_id)
