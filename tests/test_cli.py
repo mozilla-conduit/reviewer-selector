6c7383b2988e2a198a019c1a85f28e8e52d4a082
Olivier Mehani <omehani@mozilla.com>
2026-07-07 18:15:25 +1000

    tests: test environment/secrets precedence

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -141,12 +141,12 @@ def test_github_repo_added(
     sample_diff: str,
     sample_rules_data: dict[str, Any],
 ):
-    # Empty rules. The real ones should be coming from in-tree.
     rules_path = _write_rules(tmp_path / "rules.json", {})
 
     rules_resp = requests.Response()
     rules_resp.status_code = 404
     mock_fetch_rules.return_value = rules_resp
+
     mock_fetch_patch.return_value = sample_diff
 
     _run_cli(
@@ -161,6 +161,127 @@ def test_github_repo_added(
 
     assert "reviewer-selector-main" in mock_collect_reviewers.call_args[0][1], (
         "The GitHub repo name was not passed to the Rules.collect_reviewers method"
+    )
+
+
+@mock.patch("reviewer_selector.GitHubPR.fetch_rules")
+@mock.patch("reviewer_selector.GitHubPR.fetch_patch")
+@mock.patch("reviewer_selector.Rules.collect_reviewers")
+@mock.patch("reviewer_selector.github.GitHubApp")
+@mock.patch("reviewer_selector.taskcluster.TaskclusterConfig")
+@mock.patch("reviewer_selector.taskcluster.load_secrets")
+@pytest.mark.parametrize(
+    "env_app_id,env_app_privkey,env_tc_secret_id,tc_app_id,tc_app_privkey,expected_app_credentials,needs_tc_secrets",
+    (
+        ("", "", "", "", "", ("", ""), False),
+        ("", "", "", "TC_APP_ID", "TC_APP_PRIVKEY", ("", ""), False),
+        # TC secrets are used only if env missing
+        (
+            "ENV_APP_ID",
+            "ENV_APP_PRIVKEY",
+            "ENV_TC_SECRET_ID",
+            "TC_APP_ID",
+            "TC_APP_PRIVKEY",
+            ("ENV_APP_ID", "ENV_APP_PRIVKEY"),
+            False,
+        ),
+        (
+            "",
+            "",
+            "ENV_TC_SECRET_ID",
+            "TC_APP_ID",
+            "TC_APP_PRIVKEY",
+            ("TC_APP_ID", "TC_APP_PRIVKEY"),
+            True,
+        ),
+        # Some env takes priority over TC secrets
+        (
+            "ENV_APP_ID",
+            "",
+            "ENV_TC_SECRET_ID",
+            "TC_APP_ID",
+            "TC_APP_PRIVKEY",
+            ("ENV_APP_ID", "TC_APP_PRIVKEY"),
+            True,
+        ),
+        (
+            "",
+            "ENV_APP_PRIVKEY",
+            "ENV_TC_SECRET_ID",
+            "TC_APP_ID",
+            "TC_APP_PRIVKEY",
+            ("TC_APP_ID", "ENV_APP_PRIVKEY"),
+            True,
+        ),
+    ),
+)
+def test_github_env(
+    mock_tc_load_secrets: mock.Mock,
+    _mock_tc_taskclusterconfig: mock.Mock,
+    mock_github_app: mock.Mock,
+    _mock_collect_reviewers: mock.Mock,
+    mock_fetch_patch: mock.Mock,
+    mock_fetch_rules: mock.Mock,
+    tmp_path: pathlib.Path,
+    monkeypatch: pytest.MonkeyPatch,
+    mocked_github_request: Mocker,
+    capsys: pytest.CaptureFixture,
+    sample_diff: str,
+    env_app_id: str,
+    env_app_privkey: str,
+    env_tc_secret_id: str,
+    tc_app_id: str,
+    tc_app_privkey: str,
+    expected_app_credentials: tuple[str, str],
+    needs_tc_secrets: bool,
+):
+    """Test precedence between environment and TC secrets, including for incomplete data."""
+    rules_path = _write_rules(tmp_path / "rules.json", {})
+
+    monkeypatch.setenv("GITHUB_APP_ID", env_app_id)
+    monkeypatch.setenv("GITHUB_APP_PRIVKEY", env_app_privkey)
+    monkeypatch.setenv("TC_SECRET_ID", env_tc_secret_id)
+    mock_tc_load_secrets.return_value = {
+        "GITHUB_APP_ID": tc_app_id,
+        "GITHUB_APP_PRIVKEY": tc_app_privkey,
+    }
+
+    rules_resp = requests.Response()
+    rules_resp.status_code = 404
+    mock_fetch_rules.return_value = rules_resp
+
+    mock_fetch_patch.return_value = sample_diff
+
+    with mocked_github_request as mock:
+        requested_reviewers_url = "https://api.github.com/repos/mozilla-conduit/reviewer-selector/pulls/18/requested_reviewers"
+        mock_requested_reviewers = mock.post(requested_reviewers_url, text="{}")
+        _run_cli(
+            [
+                rules_path,
+                "--pr-url",
+                "https://github.com/mozilla-conduit/reviewer-selector/pull/18",
+            ],
+            "",
+            capsys,
+        )
+
+    if all(expected_app_credentials):
+        mock_github_app.assert_called_with(
+            *expected_app_credentials, "mozilla-conduit", "reviewer-selector"
+        )
+        assert mock_requested_reviewers.call_count == 1, (
+            "Incorrect number of requests to the requested reviewers endpoint"
+        )
+    else:
+        assert mock_github_app.call_count == 0, (
+            "The GitHubApp was unexpectedly initialised"
+        )
+        assert mock_requested_reviewers.call_count == 0, (
+            "Unexpected requests to the requested reviewers endpoint were made"
+        )
+
+    assert needs_tc_secrets == mock_tc_load_secrets.called, (
+        "Use of load_secrets doesn't match expectation"
     )
 
 
