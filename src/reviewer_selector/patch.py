baf5f0516315630bcd68dd362f71dcb82b839261
Olivier Mehani <omehani@mozilla.com>
2026-07-14 17:48:35 +1000

    patch: support parsing r? from subject line (bug 2023719)

--- src/reviewer_selector/patch.py
+++ src/reviewer_selector/patch.py
@@ -1,21 +1,36 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Mapping, Sequence
 import logging
+import re
 import sys
 from typing import Any, override
 
 import rs_parsepatch
 
+from reviewer_selector.review import Reviewer
+
 logger = logging.getLogger(__name__)
+
+# Note that we only allows a subset of legal IRC-nick characters.
+# Specifically, we do not allow [ \ ] ^ ` { | }
+# In addition, allow `:` to allow GitHub ent:-prefix enterprise teams.
+NICK = r"[a-zA-Z0-9\-\_.]*[a-zA-Z0-9\-\_:]+"
+GROUP_MARKER = r"#"
+BLOCKING_MARKER = r"!"
+REVIEWER = GROUP_MARKER + r"?" + NICK + BLOCKING_MARKER + r"?"
+SEPARATOR = r"[;,\/\\]\s*"
+REVIEWERS_TAG = r"r[=?](?P<reviewers>((" + SEPARATOR + ")?" + REVIEWER + ")+)"
 
 
 class Patch:
     """Wrapper around patch data, with optional patch metadata header."""
 
+    _subject: str
     _patch: str
     _parsed_diffs: Sequence[Mapping[str, Any]]
 
-    def __init__(self, diff: str):
+    def __init__(self, diff: str, subject: str = ""):
+        self._subject = subject
         self._patch = diff
         self._parse_patch()
 
@@ -29,6 +44,28 @@ class Patch:
         logger.info(f"Considering filenames: {', '.join(filenames)} ...")
 
         return filenames
+
+    def get_subject_reviewers(self) -> Iterable[Reviewer]:
+        """Parse the patch subject, looking for r[?=] patterns."""
+        subject_reviewers = self.parse_subject_reviewers(self._subject)
+
+        reviewers = []
+        for r in subject_reviewers:
+            is_group = GROUP_MARKER in r
+            is_blocking = BLOCKING_MARKER in r
+            name = r.removeprefix(GROUP_MARKER).removesuffix(BLOCKING_MARKER)
+            reviewers.append(
+                Reviewer(name=name, is_group=is_group, blocking=is_blocking)
+            )
+
+        return reviewers
+
+    @staticmethod
+    def parse_subject_reviewers(subject: str) -> Iterable[str]:
+        matches = re.search(REVIEWERS_TAG, subject)
+        if not matches:
+            return []
+        return re.split(SEPARATOR, matches.group("reviewers"))
 
 
 class PatchSource(metaclass=ABCMeta):
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,4 +1,7 @@
+import pytest
+
 from reviewer_selector.patch import Patch
+from reviewer_selector.review import Reviewer
 
 
 def test_patch_extracts_file_paths(sample_diff: str):
@@ -18,3 +21,64 @@ def test_patch_empty_diff():
     patch = Patch("")
     files = patch.get_changed_files()
     assert files == []
+
+
+@pytest.mark.parametrize(
+    "subject,expected",
+    (
+        ("a commit message", []),
+        ("a commit message r=bob", ["bob"]),
+        ("a commit message r?bob", ["bob"]),
+        ("a commit message r=#bob", ["#bob"]),
+        ("a commit message r?#bob!", ["#bob!"]),
+        ("a commit message r=bob,#alice!", ["bob", "#alice!"]),
+        (
+            "a commit message r?#ent:infra-testing-reviewers,alice!,bob",
+            ["#ent:infra-testing-reviewers", "alice!", "bob"],
+        ),
+        (
+            "a commit message r=#ent:infra-testing-reviewers,alice!,bob",
+            ["#ent:infra-testing-reviewers", "alice!", "bob"],
+        ),
+    ),
+)
+def test_parse_subject_reviewers(subject: str, expected: list[str]):
+    assert Patch.parse_subject_reviewers(subject) == expected
+
+
+@pytest.mark.parametrize(
+    "subject,expected",
+    (
+        ("a commit message", []),
+        ("a commit message r=bob", [Reviewer("bob")]),
+        ("a commit message r?bob", [Reviewer("bob")]),
+        ("a commit message r=#bob", [Reviewer("bob", is_group=True)]),
+        (
+            "a commit message r?#bob!",
+            [Reviewer("bob", is_group=True, blocking=True)],
+        ),
+        (
+            "a commit message r=bob,#alice!",
+            [Reviewer("bob"), Reviewer("alice", is_group=True, blocking=True)],
+        ),
+        (
+            "a commit message r?#ent:infra-testing-reviewers,alice!,bob",
+            [
+                Reviewer("ent:infra-testing-reviewers", is_group=True),
+                Reviewer("alice", blocking=True),
+                Reviewer("bob"),
+            ],
+        ),
+        (
+            "a commit message r=#ent:infra-testing-reviewers,alice!,bob",
+            [
+                Reviewer("ent:infra-testing-reviewers", is_group=True),
+                Reviewer("alice", blocking=True),
+                Reviewer("bob"),
+            ],
+        ),
+    ),
+)
+def test_get_subject_reviewers(subject: str, expected: list[Reviewer]):
+    patch = Patch("", subject)
+    assert patch.get_subject_reviewers() == expected
