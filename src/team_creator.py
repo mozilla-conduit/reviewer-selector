9fb36c49f1cf83d376d07996d76403373da64f43
Olivier Mehani <omehani@mozilla.com>
2026-09-03 15:34:26 +1000

    team-creator: add --dry-run

--- src/team_creator.py
+++ src/team_creator.py
@@ -22,6 +22,11 @@ def main():
         action="store_true",
         default=False,
     )
+    args_parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+    )
     args_parser.add_argument("--github-token")
     args_parser.add_argument("--organisation", default="bug2001552")
     args_parser.add_argument("rules")
@@ -48,7 +53,7 @@ def main():
         rules = json.load(f)
 
     try:
-        create_teams(client, rules, organisation, base_team)
+        create_teams(client, rules, organisation, base_team, arguments.dry_run)
     except HTTPError as exc:
         print(f"{exc} for {exc.request.body}: {exc.response.text}")
         sys.exit(3)
@@ -62,7 +67,11 @@ GitHubTeams = dict[str, list[str]]
 
 
 def create_teams(
-    client: Client, herald_rules: dict[str, Any], organisation: str, base_team: str
+    client: Client,
+    herald_rules: dict[str, Any],
+    organisation: str,
+    base_team: str,
+    dry_run: bool,
 ):
 
     logger.debug("Creating Phabricator->GitHub user map ...")
@@ -70,9 +79,7 @@ def create_teams(
     phab_github_user_map: PhabGitHubMap = {
         phab_name: github_users[phab_name]["username"] for phab_name in github_users
     }
-    logger.info(
-        f"Found {len(phab_github_user_map)} Phabricator->GitHub user mappings..."
-    )
+    logger.info(f"Found {len(phab_github_user_map)} Phabricator->GitHub user mappings")
 
     logger.info("Creating GitHub Teams member lists ...")
     groups: RulesGroups = herald_rules.get("groups", {})
@@ -92,35 +99,44 @@ def create_teams(
         teams[group_name] = members
 
     # find base team
-    ensure_team_exists(client, organisation, base_team)
-    members = get_team_members(client, organisation, base_team)
+    ensure_team_exists(client, organisation, base_team, dry_run)
+    members = get_team_members(client, organisation, base_team, dry_run)
     logger.info(f"All members of {base_team}: {members}")
 
     for team in teams:
         # create team
-        ensure_team_exists(client, organisation, team, base_team)
+        ensure_team_exists(client, organisation, team, dry_run, parent_team=base_team)
 
         # get team members
-        members = get_team_members(client, organisation, team)
+        members = get_team_members(client, organisation, team, dry_run)
         logger.info(f"Current members of {team}: {members}")
 
         # add new users
         if users_to_add := set(teams[team]) - members:
-            add_team_members(client, organisation, team, users_to_add)
+            add_team_members(client, organisation, team, users_to_add, dry_run)
 
         # remove missing users
         if members_to_remove := members - set(teams[team]):
-            remove_team_members(client, organisation, team, members_to_remove)
+            remove_team_members(client, organisation, team, members_to_remove, dry_run)
 
 
 def ensure_team_exists(
-    client: Client, organisation: str, team_name: str, parent_team: str = ""
+    client: Client,
+    organisation: str,
+    team_name: str,
+    dry_run: bool,
+    *,
+    parent_team: str = "",
 ) -> dict[str, Any]:
     logger.debug(f"Ensuring {team_name} exists ...")
     team_url = _make_team_url(organisation, team_name)
     resp: requests.Response = client.get(team_url)
 
     if resp.status_code == 404:
+        if dry_run:
+            logger.info(f"[DRY-RUN] Would create team {team_name}")
+            return {}
+
         create_payload = {
             "name": team_name,
             "description": "Super-team of all staff allowed to review changes",
@@ -135,18 +151,29 @@ def ensure_team_exists(
         resp = client.post(f"/orgs/{organisation}/teams", data=create_payload)
         logger.info(f"Created team {team_name}")
 
-    resp.raise_for_status()
+    try:
+        resp.raise_for_status()
+    except:  # noqa: E722
+        if not dry_run:
+            raise
+        logger.info(f"[DRY-RUN] Error getting {team_name} details")
+        return {}
 
     return resp.json()
 
 
 def add_team_members(
-    client: Client, organisation: str, team_name: str, users: set[str]
+    client: Client, organisation: str, team_name: str, users: set[str], dry_run: bool
 ):
     logger.debug(f"Adding users to {team_name}: {users} ...")
     add_url = f"orgs/{organisation}/teams/{team_name}/memberships/"
     add_payload = {"role": "member"}
     added_users = []
+
+    if dry_run:
+        logger.info(f"[DRY RUN] Would add users to {team_name}: {users}")
+        return
+
     for user in users:
         try:
             resp: requests.Response = client.put(add_url + user, data=add_payload)
@@ -159,11 +186,18 @@ def add_team_members(
 
 
 def remove_team_members(
-    client: Client, organisation: str, team_name: str, members: set[str]
+    client: Client, organisation: str, team_name: str, members: set[str], dry_run: bool
 ):
     logger.debug(f"Removing members from {team_name}: {members} ...")
     remove_url = f"orgs/{organisation}/teams/{team_name}/memberships/"
     removed_members = []
+
+    if dry_run:
+        logger.info(
+            f"[DRY RUN] Would remove members from {team_name}: {removed_members}"
+        )
+        return
+
     for user in members:
         try:
             # simple_github.Client.delete returns None
@@ -177,14 +211,23 @@ def remove_team_members(
     logger.info(f"Removed members from {team_name}: {removed_members}")
 
 
-def get_team_members(client: Client, organisation: str, team_name: str) -> set[str]:
+def get_team_members(
+    client: Client, organisation: str, team_name: str, dry_run: bool
+) -> set[str]:
     logger.debug(f"Getting {team_name} membership ...")
     team_url = _make_team_url(organisation, team_name)
 
     resp: requests.Response = client.get(
         f"{team_url}/members",
     )
-    resp.raise_for_status()
+
+    try:
+        resp.raise_for_status()
+    except:  # noqa: E722
+        if not dry_run:
+            raise
+        logger.info(f"[DRY-RUN] Error getting {team_name} membership, assuming empty")
+        return set()
 
     return {user["login"] for user in resp.json()}
 
