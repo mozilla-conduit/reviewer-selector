0e9f5458e2f06ac6350437658ae4a45812eb189d
Olivier Mehani <omehani@mozilla.com>
2026-09-17 18:51:47 +1000

    taskcluster: determine task URL

--- src/reviewer_selector/taskcluster.py
+++ src/reviewer_selector/taskcluster.py
@@ -1,3 +1,5 @@
+import os
+
 from taskcluster.helper import TaskclusterConfig, load_secrets
 
 
@@ -12,3 +14,10 @@ class Taskcluster:
         """Fetch a TaskCluster secret by it ID."""
         secrets = self._tc.get_service("secrets")
         return load_secrets(secrets, secret_id)
+
+
+def tc_task_url() -> str | None:
+    if (tc_root := os.getenv("TASKCLUSTER_ROOT_URL")) and (
+        tc_task_id := os.getenv("TASK_ID")
+    ):
+        return f"{tc_root}/tasks/{tc_task_id}"
